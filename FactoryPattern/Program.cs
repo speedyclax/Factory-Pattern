1de45f52915193b4ee2f4902a68aa6c2d82b0dcb
Jon Claxton <speedyclax@gmail.com>
2023-08-08 13:48:25 -0500

    Exercise completed

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter 2 or 4 for the number of tires for your vehicle:");
+            int wheelCount = int.Parse(Console.ReadLine());
+
+            var vehicle = VehicleFactory.GetVehicle(wheelCount);
+
+            vehicle.Drive();
         }
     }
 }
